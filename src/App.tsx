195454f37f0d31eccfce639351539ef008a94990
Hannah <hcour@gmail.com>
2024-05-16 16:10:48 -0400

    complete task 2

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import './App.css';
  */
 interface IState {
   data: ServerRespond[],
+  showGraph: boolean,
 }
 
 /**
@@ -21,26 +22,42 @@ class App extends Component<{}, IState> {
     this.state = {
       // data saves the server responds.
       // We use this state to parse data down to the child element (Graph) as element property
+      // hide the graph initially
       data: [],
+      showGraph: false,
     };
   }
 
   /**
    * Render Graph react component with state.data parse as property data
+   * Only renderGraph if showGraph = true
    */
   renderGraph() {
-    return (<Graph data={this.state.data}/>)
+    if (this.state.showGraph) {
+      return (<Graph data={this.state.data}/>)
+    }
+    
   }
 
   /**
    * Get new data from server and update the state with the new data
+   * Using setInterval() to allow data to be updated every 100ms.
+   * Data will update, while also maintaining existing data.
    */
   getDataFromServer() {
-    DataStreamer.getData((serverResponds: ServerRespond[]) => {
-      // Update the state by creating a new array of data that consists of
-      // Previous data in the state and the new data from server
-      this.setState({ data: [...this.state.data, ...serverResponds] });
+    let x = 0;
+    const interval = setInterval(() => {
+      DataStreamer.getData((serverResponds: ServerRespond[]) => {
+        this.setState({ 
+          data: [...this.state.data, ...serverResponds],
+          showGraph: true,
+         });
     });
+    x++;
+    if (x > 1000) {
+      clearInterval(interval);
+      }
+    }, 100);
   }
 
   /**
